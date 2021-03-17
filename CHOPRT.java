505ec44a4506e0f85a77b257fcb483716c85b8c0
dlshnkLK <dilshankaliyanage@yahoo.com>
2021-03-17 23:32:40 +0530

    solution added

---
+++ CHOPRT.java
@@ -0,0 +1,34 @@
+import java.util.;
+
+
+public class CHOPRT {
+
+	public static void main(String[] args) {
+		
+		Scanner scan =  new Scanner(System.in);
+		
+		int t = scan.nextInt();
+		
+		while(t--  0) {
+			int arr[] = new int[2];
+		
+			arr[0] = scan.nextInt();
+			arr[1] = scan.nextInt();
+		
+			if(arr[0]  arr[1]) {
+				System.out.println();
+			} else if (arr[0]  arr[1]) {
+				System.out.println();
+			} else {
+				System.out.println(=);
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+}
